a4fcc7f38120a37f71de6279f018c12a9398342b
AbelVJ <oscar.joyar@gmail.com>
2026-04-10 15:41:33 -0600

    Agregar shopping cart en dispositivos grandes

--- Frontend/Overstryde/src/components/home/Navbar.jsx
+++ Frontend/Overstryde/src/components/home/Navbar.jsx
@@ -9,7 +9,12 @@ import {
     navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
 import { Input } from "../common/Input";
-import { Heart, ShoppingBag, User } from "lucide-react";
+import { Heart, ShoppingBag, User, X } from "lucide-react";
+import { CustomDrawer } from "../common/CustomDrawer";
+import { CustomDrawerHeader } from "../common/CustomDrawerHeader";
+import { CustomDrawerFooter } from "../shoppingCart/ShoppingDrawerFooter";
+import { DrawerClose } from "../ui/drawer";
+import { ShoppingCart } from "../shoppingCart/ShoppingCart";
 
 const items = [
     {
@@ -96,9 +101,11 @@ export const Navbar = () => {
                     <NavLink to={"/register"} className={""}>
                         <User size={25} className="transition-transform duration-300 hover:-translate-y-1" />
                     </NavLink>
-                    <NavLink>
-                        <ShoppingBag size={25} className="transition-transform duration-300 hover:-translate-y-1" />
-                    </NavLink>
+                    <CustomDrawer direction={"right"} icon={<ShoppingBag size={25} className="transition-transform duration-300 hover:-translate-y-1 text-black" />} header={<CustomDrawerHeader  title={"Carrito de compras"} description={"Todas las prendas que tu escojas estarán aca"} icon={<DrawerClose>
+                        <X size={25} />
+                    </DrawerClose>
+                    } />} content={<ShoppingCart />
+                    } footer={<CustomDrawerFooter className={"shadow-md"} />} />
                 </div>
             </div>
 
