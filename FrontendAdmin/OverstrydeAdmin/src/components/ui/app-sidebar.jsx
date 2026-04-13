c4be49bb6d9fa66ff0d7edb6a790384af4d62843
MaxJS07 <maxjimenezsep@gmail.com>
2026-04-12 22:47:04 -0600

    Feat: Se aplicó el diseño del sidebar (footer faltante)

--- FrontendAdmin/OverstrydeAdmin/src/components/ui/app-sidebar.jsx
+++ FrontendAdmin/OverstrydeAdmin/src/components/ui/app-sidebar.jsx
@@ -9,27 +9,37 @@ import {
     SidebarMenuButton,
     SidebarHeader,
     SidebarGroupLabel,
-    SidebarFooter
+    SidebarFooter,
+    SidebarRail
 } from "@/components/ui/sidebar";
-import { LayoutDashboard, ClipboardList, PackageSearch, FolderOpen, Image } from "lucide-react";
+import { LayoutDashboard, ClipboardList, PackageSearch, FolderOpen, Image, EllipsisVertical, LogOut } from "lucide-react";
 
 export function AppSidebar() {
 
     const location = useLocation();
 
     return (
-        <Sidebar collapsible="icon" className="border-r">
-            <SidebarHeader className="group-data-[collapsible=icon]:hidden p-4 text-lg font-semibold">OVERSTRYD</SidebarHeader>
+        <Sidebar collapsible="icon" className="border-r data-[collapsible=icon]:flex data-[collapsible=icon]:justify-center">
+            <SidebarHeader className=" flex flex-row items-center">
+                <img src="/overstryde-isotipo-negro.jpeg" alt="" className="h-10 w-10 ml-1 group-data-[collapsible=icon]:ml-0 " />
+                <span className="p-6 text-xl font-semibold sidebarTitle group-data-[collapsible=icon]:hidden m-0 justify-start !font-inter">
+                    OVERSTRYD
+                </span>
+            </SidebarHeader>
             <SidebarContent >
                 <SidebarGroup>
                     <SidebarGroupContent>
                         <SidebarMenu>
 
                             <SidebarMenuItem>
-                                <SidebarMenuButton asChild isActive={location.pathname === "/"}>
+                                <SidebarMenuButton
+                                    asChild
+                                    isActive={location.pathname === "/"}
+                                    className="h-12 data-[active=true]:text-yellow-400"
+                                >
                                     <Link to={"/"}>
-                                        <LayoutDashboard className="mr-2 h-4 w-4"></LayoutDashboard>
-                                        <span className="group-data-[collapsible=icon]:hidden">
+                                        <LayoutDashboard className="mr-2 !w-6 !h-6 shrink-0"></LayoutDashboard>
+                                        <span className="group-data-[collapsible=icon]:hidden font-inter">
                                             Dashboard
                                         </span>
                                     </Link>
@@ -37,10 +47,14 @@ export function AppSidebar() {
                             </SidebarMenuItem>
 
                             <SidebarMenuItem>
-                                <SidebarMenuButton asChild isActive={location.pathname === "/orders"} >
+                                <SidebarMenuButton
+                                    asChild
+                                    isActive={location.pathname === "/orders"}
+                                    className="h-12 data-[active=true]:text-yellow-400"
+                                >
                                     <Link to={"/orders"} >
-                                        <ClipboardList className="mr-2 h-4 w-4" />
-                                        <span className="group-data-[collapsible=icon]:hidden">
+                                        <ClipboardList className="mr-2 !w-6 !h-6" />
+                                        <span className="group-data-[collapsible=icon]:hidden font-inter">
                                             Órdenes
                                         </span>
                                     </Link>
@@ -48,21 +62,29 @@ export function AppSidebar() {
                             </SidebarMenuItem>
 
                             <SidebarMenuItem>
-                                <SidebarMenuButton asChild isActive={location.pathname === "/products"} >
+                                <SidebarMenuButton
+                                    asChild
+                                    isActive={location.pathname === "/products"}
+                                    className="h-12 data-[active=true]:text-yellow-400"
+                                >
                                     <Link to={"/products"}>
-                                        <PackageSearch className="mr-2 h-4 w-4" />
-                                        <span className="group-data-[collapsible=icon]:hidden">
-                                            Products
+                                        <PackageSearch className="mr-2 !w-6 !h-6" />
+                                        <span className="group-data-[collapsible=icon]:hidden font-inter">
+                                            Productos
                                         </span>
                                     </Link>
                                 </SidebarMenuButton>
                             </SidebarMenuItem>
 
                             <SidebarMenuItem>
-                                <SidebarMenuButton asChild isActive={location.pathname === "/categories"} >
+                                <SidebarMenuButton
+                                    asChild
+                                    isActive={location.pathname === "/categories"}
+                                    className="h-12 data-[active=true]:text-yellow-400"
+                                >
                                     <Link to={"/categories"}>
-                                        <FolderOpen className="mr-2 h-4 w-4" />
-                                        <span className="group-data-[collapsible=icon]:hidden">
+                                        <FolderOpen className="mr-2 !w-6 !h-6" />
+                                        <span className="group-data-[collapsible=icon]:hidden font-inter">
                                             Categorías
                                         </span>
                                     </Link>
@@ -70,10 +92,14 @@ export function AppSidebar() {
                             </SidebarMenuItem>
 
                             <SidebarMenuItem>
-                                <SidebarMenuButton asChild isActive={location.pathname === "/banners"} >
+                                <SidebarMenuButton
+                                    asChild
+                                    isActive={location.pathname === "/banners"}
+                                    className="h-12 data-[active=true]:text-yellow-400"
+                                >
                                     <Link to={"/banners"}>
-                                        <Image className="mr-2 h-4 w-4" />
-                                        <span className="group-data-[collapsible=icon]:hidden">
+                                        <Image className="mr-2 !w-6 !h-6 group-data-[collapsible=icon]:mr-0" />
+                                        <span className="group-data-[collapsible=icon]:hidden font-inter">
                                             Banners
                                         </span>
                                     </Link>
@@ -83,9 +109,36 @@ export function AppSidebar() {
                     </SidebarGroupContent>
                 </SidebarGroup>
             </SidebarContent>
-            <SidebarFooter className="group-data-[collapsible=icon]:hidden">
-                2026
+            <SidebarFooter className="cursor-pointer " >
+
+                <div className="hover:!bg-gray-800 flex justify-center m-0">
+                    <img src="https://thumbs.dreamstime.com/b/foto-de-perfil-un-hombre-cauc%C3%A1sico-sonriente-los-a%C3%B1os-con-gafas-del-joven-y-feliz-en-espect%C3%A1culos-muestra-confianza-liderazgo-196716547.jpg" alt="" className="h-9 w-9 object-cover rounded-full hidden group-data-[collapsible=icon]:block ring-white" />
+                </div>
+
+                {/* <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <div className=" sidebarFooter flex flex-row items-center justify-start p-3 rounded-xl ring-2 ring-gray-700 gap-3 mx-1 group-data-[collapsible=icon]:hidden hover:!bg-gray-800 transition-colors duration-200">
+                            <img src="https://thumbs.dreamstime.com/b/foto-de-perfil-un-hombre-cauc%C3%A1sico-sonriente-los-a%C3%B1os-con-gafas-del-joven-y-feliz-en-espect%C3%A1culos-muestra-confianza-liderazgo-196716547.jpg" alt="" className="h-10 w-10 object-cover rounded-full" />
+                            <div className="flex flex-col m-0">
+                                <p className="font-bold text-xs">Max Jiménez</p>
+                                <p className="text-xs">20240071@gmail.com</p>
+                            </div>
+                            <EllipsisVertical className="!w-4 !h-4 shrink-0" />
+                        </div>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="w-40" align="start">
+                        <DropdownMenuGroup>
+                            <DropdownMenuItem>
+                                <LogOut />
+                                Cerrar sesión
+                            </DropdownMenuItem>
+                        </DropdownMenuGroup>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+                */}
+
+
             </SidebarFooter>
         </Sidebar>
     )
-}
+} 
