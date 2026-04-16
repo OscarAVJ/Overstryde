caafefa13a75374deb1df92e491a39383765b16b
AbelVJ <oscar.joyar@gmail.com>
2026-04-16 07:53:47 -0600

    Inicio de contact page

--- Frontend/Overstryde/src/pages/ContactPage.jsx
+++ Frontend/Overstryde/src/pages/ContactPage.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export const ContactPage = () => {
   return (
-    <div>ContactPage</div>
+    <div className='py-14 max-w-6xl'>
+        <h1>jkdjfkds</h1>
+    </div>
   )
 }
