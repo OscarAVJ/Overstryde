c7b7570e5658f60f86fa78e6439be620838236e9
AbelVJ <oscar.joyar@gmail.com>
2026-04-17 21:34:06 -0600

    Agregar datos confiables

--- Frontend/Overstryde/src/components/userProfile/AccordionItemProfile.jsx
+++ Frontend/Overstryde/src/components/userProfile/AccordionItemProfile.jsx
@@ -3,37 +3,47 @@ import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '..
 import { ShoppingCartProduct } from '../shoppingCart/ShoppingCartProduct'
 
 export const AccordionItemUser = ({ value, details }) => {
-  return (
-    <AccordionItem value={value}>
-      <AccordionTrigger>Compra 00001</AccordionTrigger>
-
-      <AccordionContent className="pt-2">
-        <h1>
-          jkjskjskjskjskjskjksjksjksjksjs
-          jkjskjskjskjskjskjksjksjksjksjssjksjksj
-          skjskjs
-        </h1>
-
-        <Accordion
-          type="single"
-          collapsible
-          className="w-full p-3"
-        >
-          <AccordionItem value={details}>
-            <AccordionTrigger className="text-yellow-500">
-              Ver más detalle
-            </AccordionTrigger>
+    return (
+        <AccordionItem value={value}>
+            <AccordionTrigger>Compra 00001</AccordionTrigger>
 
             <AccordionContent className="pt-2">
-              <div className="flex flex-col gap-2">
-                {Array.from({ length: 3 }).map((_, i) => (
-                  <ShoppingCartProduct key={i} isShoppingCart={false} />
-                ))}
-              </div>
+                <p>
+                    Dirección: Casa de Laura
+                </p>
+                <p>
+                    Cantidad de productos: 15
+                </p>
+                <p>
+                    SubTotal: $500
+                </p>        
+                <p>
+                    Decuentos: $100
+                </p>               
+                <p>
+                    Total: $400
+                </p>
+
+                <Accordion
+                    type="single"
+                    collapsible
+                    className="w-full px-3"
+                >
+                    <AccordionItem value={details}>
+                        <AccordionTrigger className="text-yellow-500">
+                            Ver más detalle
+                        </AccordionTrigger>
+
+                        <AccordionContent className="pt-2">
+                            <div className="flex flex-col gap-2">
+                                {Array.from({ length: 3 }).map((_, i) => (
+                                    <ShoppingCartProduct key={i} isShoppingCart={false} />
+                                ))}
+                            </div>
+                        </AccordionContent>
+                    </AccordionItem>
+                </Accordion>
             </AccordionContent>
-          </AccordionItem>
-        </Accordion>
-      </AccordionContent>
-    </AccordionItem>
-  );
+        </AccordionItem>
+    );
 };
