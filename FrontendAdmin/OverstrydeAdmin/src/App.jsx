60778f265b59779ff22bd826c714fe9376097252
MaxJS07 <maxjimenezsep@gmail.com>
2026-04-18 20:12:33 -0600

    Feat: Layout para login y recuperar contraseña listo

--- FrontendAdmin/OverstrydeAdmin/src/App.jsx
+++ FrontendAdmin/OverstrydeAdmin/src/App.jsx
@@ -1,23 +1,33 @@
 import { SidebarProvider, SidebarTrigger, Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarHeader } from "./components/ui/sidebar"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
+import { AuthLayout } from "./AuthLayout";
 import Products from "./pages/Products";
 import Orders from "./pages/Orders";
 import Dashboard from "./pages/Dashboard";
 import Categories from "./pages/Categories";
 import Banners from "./pages/Banners";
+import Login from "./pages/Login"
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
 
+        {/*Layout para login y recuperar contraseña */}
+        <Route path="/auth" element={<AuthLayout/>}>
+          <Route path="login" element={<Login/>}/>
+          <Route path="forgotPassword" element={<ForgotPassword/>}/>
+        </Route>
+
+        {/*Layout principal*/}
         <Route path="/" element={<Layout />}>
           <Route index element={<Dashboard/>} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/products" element={<Products/>}/>
-          <Route path="/categories" element={<Categories/>}/>
-          <Route path="/banners" element={<Banners/>}/>
+          <Route path="orders" element={<Orders />} />
+          <Route path="products" element={<Products/>}/>
+          <Route path="categories" element={<Categories/>}/>
+          <Route path="banners" element={<Banners/>}/>
         </Route>
 
       </Routes>
