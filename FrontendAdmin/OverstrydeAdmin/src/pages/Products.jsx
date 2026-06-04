09b7788f70cff65cf2bf20690ba504aa1cb6241f
MaxJS07 <maxjimenezsep@gmail.com>
2026-06-04 11:10:14 -0600

    Fix: Arreglos de lógica en el modal de productos

--- FrontendAdmin/OverstrydeAdmin/src/pages/Products.jsx
+++ FrontendAdmin/OverstrydeAdmin/src/pages/Products.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from '@/components/ui/button'
-import { Plus, FolderOpen, Search, Pencil, Trash2 } from 'lucide-react'
+import { Plus, FolderOpen, Search, Pencil, Trash2, PlusIcon, Dot, X } from 'lucide-react'
 import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card2"
 import { Field, FieldDescription, FieldLabel, FieldGroup } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
@@ -11,57 +11,54 @@ import { Separator } from "@/components/ui/separator"
 import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
 import { Label } from "@/components/ui/label"
 import { Textarea } from '@/components/ui/textarea'
+import { Switch } from '@/components/ui/switch'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { Command, CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from '@/components/ui/command'
+import { Item, ItemActions, ItemContent, ItemDescription, ItemMedia, ItemTitle } from "@/components/ui/item"
 import useProducts from '@/hooks/useProducts'
 
 const Products = () => {
 
-  const {products} = useProducts();
+  const { products } = useProducts();
+  const [openCategories, setOpenCategories] = useState(false)
+  const [selectedCategories, setSelectedCategories] = useState([])
 
-  {/*const products = [
+  const selected = [
     {
-      name: "Camiseta Deportiva",
-      sku: "TSH001",
-      category: "Hombres > Camisas",
-      price: 25.99,
-      stock: 45,
+      category: "Hombres",
+      sub: "Camisa"
     },
     {
-      name: "Leggings de Yoga",
-      sku: "LEG002",
-      category: "Mujeres > Leggings",
-      price: 39.99,
-      stock: 8,
+      category: "Mujeres",
+      sub: "Blusas"
+    }
+  ]
+
+  const categories = [
+    {
+      name: "Hombres",
+      subcategories: [
+        "Camisas",
+        "Camisetas",
+        "Pants"
+      ]
     },
     {
-      name: "Mancuernas 5kg",
-      sku: "DUM003",
-      category: "Accesorios > Mancuernas",
-      price: 49.99,
-      stock: 0,
-    },
-    {
-      name: "Mancuernas 10kg",
-      sku: "DUM004",
-      category: "Accesorios > Mancuernas",
-      price: 49.99,
-      stock: 0,
-    },
-    {
-      name: "Guantes",
-      sku: "DUM003",
-      category: "Accesorios > Guantes",
-      price: 49.99,
-      stock: 0,
-    },
-  ];*/}
+      name: "Mujeres",
+      subcategories: [
+        "Blusas",
+        "Pantalones",
+        "Vinchas"
+      ]
+    }
+  ]
+
 
   function getStatus(stock) {
     if (stock === 0) return { label: "Sin stock", variant: "destructive" };
     if (stock < 10) return { label: "Poco stock", variant: "secondary" };
     return { label: "En stock", variant: "default" };
   }
-
-
 
   return (
     <div className='space-y-6 min-h-screen p-6 rounded-lg'>
@@ -80,64 +77,173 @@ const Products = () => {
                 </Button>
               </DialogTrigger>
               <DialogContent className="sm:max-w-4xl max-h-[90vh] overflow-y-auto">
-                <DialogHeader>
-                  <DialogTitle className="text-2xl font-bold">Agregar nuevo producto</DialogTitle>
-                  <DialogDescription>Complete todos los datos del nuevo producto</DialogDescription>
+                <DialogHeader className="flex flex-col gap-4 sm:gap-0 sm:flex-row justify-between items-center">
+                  <div>
+                    <DialogTitle className="text-2xl font-bold">Agregar nuevo producto</DialogTitle>
+                    <DialogDescription>Complete todos los datos del nuevo producto</DialogDescription>
+                  </div>
                 </DialogHeader>
                 <Separator></Separator>
-                <div className='grid grid-cols-1 sm:grid-cols-3 gap-6 p-2'>
-                  <div className='sm:col-span-2'>
-                    <FieldGroup>
-                      <Field>
-                        <FieldLabel htmlFor="productName">Nombre del producto<span className='text-red-500'>*</span></FieldLabel>
-                        <Input id="productName" name="name" placeholder="Introduzca nombre del producto..." />
-                      </Field>
-                      <Field>
-                        <FieldLabel htmlFor="productCategory">Categoría</FieldLabel>
-                        <Select>
-                          <SelectTrigger id="productCategory" defaultValue="">
-                            <SelectValue placeholder="Elegir categoría" />
-                          </SelectTrigger>
-                          <SelectContent>
-                            <SelectGroup>
-                              <SelectItem value="1">Categoría 1</SelectItem>
-                              <SelectItem value="2">Categoría 2</SelectItem>
-                              <SelectItem value="3">Categoría 3</SelectItem>
-                            </SelectGroup>
-                          </SelectContent>
-                        </Select>
-                      </Field>
-                      <Field>
-                        <FieldLabel htmlFor="productDesc">Descripción</FieldLabel>
-                        <Textarea id="productDesc" placeholder="Descripción del producto..."></Textarea>
-                      </Field>
-                      <div className='flex flex-row gap-2'>
-                        <Field>
-                          <FieldLabel htmlFor="productPrice">Precio</FieldLabel>
-                          <Input id="productPrice" type="number"></Input>
-                        </Field>
-                        <Field>
-                          <FieldLabel htmlFor="productStock">Stock</FieldLabel>
-                          <Input id="productStock" type="number"></Input>
-                        </Field>
-                      </div>
-                    </FieldGroup>
-                  </div>
+                <Tabs>
+                  <TabsList>
+                    <TabsTrigger value="general">Producto general</TabsTrigger>
+                    <TabsTrigger value="clothes">Ropa</TabsTrigger>
+                  </TabsList>
 
-                  <div className="flex items-center justify-center h-full">
-                    <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 bg-neutral-secondary-medium border border-dashed border-default-strong rounded-base cursor-pointer hover:bg-neutral-tertiary-medium">
-                      <div className="flex flex-col items-center justify-center text-body pt-5 pb-6">
-                        <svg className="w-8 h-8 mb-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h3a3 3 0 0 0 0-6h-.025a5.56 5.56 0 0 0 .025-.5A5.5 5.5 0 0 0 7.207 9.021C7.137 9.017 7.071 9 7 9a4 4 0 1 0 0 8h2.167M12 19v-9m0 0-2 2m2-2 2 2" /></svg>
-                        <p className="mb-2 text-sm"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                        <p className="text-xs">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                  {/*TAB DE PRODUCTO GENERAL */}
+                  <TabsContent value="general">
+                    <div className='grid grid-cols-1 sm:grid-cols-3 gap-6 p-2'>
+                      <div className='sm:col-span-2'>
+                        <FieldGroup>
+                          <Field>
+                            <FieldLabel htmlFor="productName">Nombre del producto<span className='text-red-500'>*</span></FieldLabel>
+                            <Input id="productName" name="name" placeholder="Introduzca nombre del producto..." />
+                          </Field>
+                          <div className='flex flex-row gap-2'>
+                            <FieldLabel>Categorías</FieldLabel>
+                            <Button className="bg-gray-300 text-black h-6 text-sm" onClick={() => setOpenCategories(true)}><PlusIcon className='h-4 w-4' />Añadir</Button>
+                            <CommandDialog open={openCategories} onOpenChange={setOpenCategories}>
+                              <Command>
+                                <CommandInput placeholder="Buscar categoría por nombre." />
+                                <CommandList>
+                                  <CommandEmpty>No se encontraron resultados.</CommandEmpty>
+                                  {categories.map((category, index) => (
+                                    <CommandGroup heading={category.name} key={index}>
+                                      {category.subcategories.map((sub, index) => (
+                                        <CommandItem key={index}>{sub}</CommandItem>
+                                      ))}
+                                    </CommandGroup>
+                                  ))}
+                                </CommandList>
+                              </Command>
+                            </CommandDialog>
+                          </div>
+                          <div className='flex flex-col p-1 gap-1 rounded-md'>
+                            {selected.map((sel, index) => (
+                              <Item variant='outline' size='sm' key={index}>
+                                <ItemMedia>
+                                  <Dot />
+                                </ItemMedia>
+                                <ItemContent className="flex justify-center items-start">
+                                  <ItemTitle >{sel.category + " > " + sel.sub}</ItemTitle>
+                                </ItemContent>
+                                <ItemActions>
+                                  <button>
+                                    <X className="size-4 cursor-pointer" />
+                                  </button>
+                                </ItemActions>
+                              </Item>
+                            ))}
+                          </div>
+                          <Field>
+                            <FieldLabel htmlFor="productDesc">Descripción</FieldLabel>
+                            <Textarea id="productDesc" placeholder="Descripción del producto..."></Textarea>
+                          </Field>
+                          <div className='flex flex-row gap-2'>
+                            <Field>
+                              <FieldLabel htmlFor="productPrice">Precio</FieldLabel>
+                              <Input id="productPrice" type="number" placeholder="$0.00"></Input>
+                            </Field>
+                            <Field>
+                              <FieldLabel htmlFor="productStock">Stock</FieldLabel>
+                              <Input id="productStock" type="number" placeholder="0"></Input>
+                            </Field>
+                          </div>
+                        </FieldGroup>
                       </div>
-                      <input id="dropzone-file" type="file" className="hidden" />
-                    </label>
-                  </div>
-                </div>
-                
+
+                      <div className="flex items-center justify-center h-full">
+                        <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 bg-neutral-secondary-medium border border-dashed border-default-strong rounded-base cursor-pointer hover:bg-neutral-tertiary-medium">
+                          <div className="flex flex-col items-center justify-center text-body pt-5 pb-6">
+                            <svg className="w-8 h-8 mb-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h3a3 3 0 0 0 0-6h-.025a5.56 5.56 0 0 0 .025-.5A5.5 5.5 0 0 0 7.207 9.021C7.137 9.017 7.071 9 7 9a4 4 0 1 0 0 8h2.167M12 19v-9m0 0-2 2m2-2 2 2" /></svg>
+                            <p className="mb-2 text-sm"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                            <p className="text-xs">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                          </div>
+                          <input id="dropzone-file" type="file" className="hidden" />
+                        </label>
+                      </div>
+                    </div>
+                  </TabsContent>
+
+                  {/* TAB DE PRODUCTOS DE ROPA */}
+                  <TabsContent value="clothes">
+                    <div className='grid grid-cols-1 sm:grid-cols-3 gap-6 p-2'>
+                      <div className='sm:col-span-2'>
+                        <FieldGroup>
+                          <Field>
+                            <FieldLabel htmlFor="productName">Nombre del producto<span className='text-red-500'>*</span></FieldLabel>
+                            <Input id="productName" name="name" placeholder="Introduzca nombre del producto..." />
+                          </Field>
+                          <div className='flex flex-row gap-2'>
+                            <FieldLabel>Categorías</FieldLabel>
+                            <Button className="bg-gray-300 text-black h-6 text-sm" onClick={() => setOpenCategories(true)}><PlusIcon className='h-4 w-4' />Añadir</Button>
+                            <CommandDialog open={openCategories} onOpenChange={setOpenCategories}>
+                              <Command>
+                                <CommandInput placeholder="Buscar categoría por nombre." />
+                                <CommandList>
+                                  <CommandEmpty>No se encontraron resultados.</CommandEmpty>
+                                  {categories.map((category, index) => (
+                                    <CommandGroup heading={category.name} key={index}>
+                                      {category.subcategories.map((sub, index) => (
+                                        <CommandItem key={index}>{sub}</CommandItem>
+                                      ))}
+                                    </CommandGroup>
+                                  ))}
+                                </CommandList>
+                              </Command>
+                            </CommandDialog>
+                          </div>
+                          <div className='flex flex-col p-1 gap-1 rounded-md'>
+                            {selected.map((sel, index) => (
+                              <Item variant='outline' size='sm' key={index}>
+                                <ItemMedia>
+                                  <Dot />
+                                </ItemMedia>
+                                <ItemContent className="flex justify-center items-start">
+                                  <ItemTitle >{sel.category + " > " + sel.sub}</ItemTitle>
+                                </ItemContent>
+                                <ItemActions>
+                                  <button>
+                                    <X className="size-4 cursor-pointer" />
+                                  </button>
+                                </ItemActions>
+                              </Item>
+                            ))}
+                          </div>
+                          <Field>
+                            <FieldLabel htmlFor="productDesc">Descripción</FieldLabel>
+                            <Textarea id="productDesc" placeholder="Descripción del producto..."></Textarea>
+                          </Field>
+                          <div className='flex flex-row gap-2'>
+                            <Field>
+                              <FieldLabel htmlFor="productPrice">Precio</FieldLabel>
+                              <Input id="productPrice" type="number" placeholder="$0.00"></Input>
+                            </Field>
+                            <Field>
+                              <FieldLabel htmlFor="productStock">Stock</FieldLabel>
+                              <Input id="productStock" type="number" placeholder="0"></Input>
+                            </Field>
+                          </div>
+                        </FieldGroup>
+                      </div>
+
+                      <div className="flex items-center justify-center h-full">
+                        <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 bg-neutral-secondary-medium border border-dashed border-default-strong rounded-base cursor-pointer hover:bg-neutral-tertiary-medium">
+                          <div className="flex flex-col items-center justify-center text-body pt-5 pb-6">
+                            <svg className="w-8 h-8 mb-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h3a3 3 0 0 0 0-6h-.025a5.56 5.56 0 0 0 .025-.5A5.5 5.5 0 0 0 7.207 9.021C7.137 9.017 7.071 9 7 9a4 4 0 1 0 0 8h2.167M12 19v-9m0 0-2 2m2-2 2 2" /></svg>
+                            <p className="mb-2 text-sm"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                            <p className="text-xs">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                          </div>
+                          <input id="dropzone-file" type="file" className="hidden" />
+                        </label>
+                      </div>
+                    </div>
+                  </TabsContent>
+                </Tabs>
+
+
                 <DialogFooter>
-                  <DialogClose asChild> 
+                  <DialogClose asChild>
                     <Button variant='outline'>Cancelar</Button>
                   </DialogClose>
                   <Button>Guardar</Button>
